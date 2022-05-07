9159449c5dd6ed06b4318327f824a8bbb170cf9f
Andriy Koziy <andriy@koziy.com>
2022-05-07 22:40:37 +0300

    fix misprint

--- ajax_select/fields.py
+++ ajax_select/fields.py
@@ -75,10 +75,10 @@ class AutoCompleteSelectWidget(forms.widgets.TextInput):
         initial = None
         lookup = registry.get(self.channel)
         if value:
+            if isinstance(value, Model):
+                value = value.pk
             objs = lookup.get_objects([value])
             try:
-                if isinstance(value, Model):
-                    value = value.pk
                 obj = objs[0]
             except IndexError:
                 # raise Exception(f"{lookup} cannot find object:{value}")
